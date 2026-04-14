669d8bce55d5ba7e57bb0bd126a800423ccae102
Stepan Stepanyan <stepanyanstepan00@gmail.com>
2026-04-14 23:36:59 +0400

    dd

---
+++ pyhton_modul_02/ft_first_exception.py
@@ -0,0 +1,22 @@
+def input_temperature(temp_str: str) -> int:
+    print(f"Input data is {temp_str}")
+    return int(temp_str)
+
+def test_temperature():
+    print("=== Garden Temperature ===")
+    try:
+        x = input_temperature('25')
+        print(f"Temperature is now {x}°C")
+    except Exception as e:
+        print(f"Caught input_temperature error: {e}")
+    try:
+        x = input_temperature('abc')
+        print(f"Temperature is now {x}°C")
+    except Exception as e:
+        print(f"Caught input_temperature error: {e}")
+    print("All tests completed - program didn't crash!")
+
+
+if __name__ == "__main__":
+          test_temperature()
+
---
+++ pyhton_modul_02/ft_raise_exception.py
@@ -0,0 +1,27 @@
+def input_temperature(temp_str: str) -> int:
+    print(f"Input data is {temp_str}")
+    numb = int(temp_str)
+    if numb >=0 and numb <= 40:
+            return numb
+    elif numb > 40:
+        raise ValueError(f"{numb}°C is too hot for plants (max 40°C)")
+    elif numb < 0:
+        raise ValueError(f"{numb}°C is too cold for plants (min 0°C)")
+        
+    else:
+        return int(temp_str)
+
+def test_temperature():
+    print("=== Garden Temperature ===")
+    for i in ['25','abc','100','-50']:
+        try:
+            z = input_temperature(i)
+            print(f"Temperature is now {z}°C")
+        except Exception as e:
+            print(f"Caught input_temperature error: {e}")
+    print("All tests completed - program didn't crash!")
+
+
+if __name__ == "__main__":
+          test_temperature()
+
