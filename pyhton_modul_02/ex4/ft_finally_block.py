4234d3d52609efbd49fb6d32b301a31dd30a0ea4
Stepan Stepanyan <stepanyanstepan00@gmail.com>
2026-04-19 16:05:48 +0400

    everythin almost done

--- pyhton_modul_02/ex4/ft_finally_block.py
+++ pyhton_modul_02/ex4/ft_finally_block.py
@@ -12,30 +12,41 @@ class WaterError(GardenError):
     def __init__(self, message: str) -> None:
         GardenError.__init__(self, message)
 
-def water_plant(plant_name: str):
-    try:
-        if(plant_name == str.capitalize(plant_name)):
-            print(f"Watering {plant_name}:[OK]")
-        else:
-            raise PlantError("Invalid plant name to water")
-    except PlantError as e:
-        print(f"{e}")
 
-def test_watering_system():
+def water_plant(plant_name: str) -> None:
+    if (plant_name == str.capitalize(plant_name)):
+        print(f"Watering {plant_name}:[OK]")
+    else:
+        raise PlantError("Invalid plant name to water")
+
+
+def test_watering_system() -> None:
     print("=== Garden Watering System ===")
     print("Testing valid plants...")
     print("Opening watering system")
-    x = ["Tomato","lettuce","Carrots"]
-    for i in x:
-        try:
+    x = ["Tomato", "Lettuce", "Carrots"]
+    try:
+        for i in x:
             water_plant(i)
-        except PlantError as e:
-            str.capitalize(i)
-        finally:
+    except PlantError as e:
+        print(f"Caught PlantError: {e}: {i}", end='\n')
+        print(".. ending tests and returning to main")
+    finally:
+        print("Closing watering system")
+    print("\n")
+    print("Testing valid plants...")
+    print("Opening watering system")
+    x[1] = "lettuce"
+    try:
+        for i in x:
             water_plant(i)
-
+    except PlantError as e:
+        print(f"Caught PlantError: {e}: {i}", end="\n")
+        print(".. ending tests and returning to main")
+    finally:
+        print("Closing watering system")
+    print("Cleanup always happens, even with errors!")
 
 
 if __name__ == "__main__":
     test_watering_system()
-
